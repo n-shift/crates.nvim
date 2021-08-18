95469218883170bb48afc4e7110991eda9cc5f37
shift-d <me@shift-d.tk>
2021-08-18 12:39:59 +0300

    chore: create utils.is_cargo_toml function

--- lua/crates/utils.lua
+++ lua/crates/utils.lua
@@ -1,3 +1,14 @@
 local Module = {}
 
+Module.is_cargo_toml = function()
+    local current_buffer_name = vim.api.nvim_buf_get_name(0)
+    local result = string.match(current_buffer_name, "Cargo.toml$")
+
+    if result then
+        return true
+    end
+
+    return false
+end
+
 return Module
