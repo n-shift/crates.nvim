eeb93ac3b60e31ba0644aa4ee67b59ed55678604
shift-d <me@shift-d.tk>
2021-08-18 13:01:13 +0300

    chore: create constants.lua file

---
+++ lua/crates/constants.lua
@@ -0,0 +1,23 @@
+local Module = {}
+
+Module.DEFAULT_OPTIONS = {
+    colors = {
+        up_to_date = "#3C4048",
+        outdated   = "#d19a66",
+    },
+    icons = {
+        enable = true,
+        style = {
+            up_to_date = "|  ",
+            outdated   = "|  ",
+        },
+    },
+    autostart = true,
+}
+
+Module.HIGHLIGHT_GROUPS = {
+    outdated   = "CratesOutdatedVersion",
+    up_to_date = "CratesUpToDateVersion",
+}
+
+return Module
