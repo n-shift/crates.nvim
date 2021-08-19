f5db2f39c7959e20ba3bd77dc48a470feef7011f
shift-d <me@shift-d.tk>
2021-08-19 11:35:24 +0300

    chore: base init.lua for debugging plugin

--- lua/crates/init.lua
+++ lua/crates/init.lua
@@ -1,3 +1,9 @@
+local ui     = require("crates.ui")
+local api    = require("crates.api")
+local utils  = require("crates.utils")
+local buffer = require("crates.buffer")
+local config = require("crates.config")
+
 local Module = {}
 
 return Module
