78d16511cb54029557f4dfa58f807fa96e6934cb
shift-d <me@shift-d.tk>
2021-08-19 14:01:37 +0300

    feat: fetch latest crate version

--- lua/crates/api.lua
+++ lua/crates/api.lua
@@ -1,15 +1,22 @@
-local json = require("imports.json")
-local curl = require("plenary.curl")
+local curl = require('plenary.curl')
+local json = require('imports.json')
 
 local Module = {}
 
-function Module.get_current_dependency_version(name)
-    local url = table.concat({"https://crates.io/api/v1/crates/", name, callback = function(res)
-        return res
-    end
-    })
-    local raw = curl.get(url, { accept = "application/json", compressed = false, dump = {""} })
-    return json.decode(raw).crate.max_stable_version
+function Module.get_current_dependency_version(name, unstable)
+    local url = table.concat({"https://crates.io/api/v1/crates/", name})
+    body = curl.request({
+        url = url,
+	method = "get",
+	accepts = "application/json",
+	compressed = false,
+	timeout = 900000
+    }).body
+    
+    local crate_info = json.decode(body).crate
+
+    if unstable then return crate_info.max_version else return crate_info.max_stable_version end
+
 end
 
 return Module
