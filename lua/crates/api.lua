5ffeeeb31cf589c5b7598c2a5c89edd02c9d5a56
shift-d <me@shift-d.tk>
2021-08-19 11:34:58 +0300

    feat: get current dependency version

--- lua/crates/api.lua
+++ lua/crates/api.lua
@@ -1,28 +1,12 @@
-local json = require("crates.imports.json")
+local json = require("imports.json")
+local curl = require("plenary.curl")
 
 local Module = {}
 
-function Module:get_outdated_dependencies(callback)
-    local command = "cargo outdated --format json"
-    local finished = false
-
-    vim.fn.jobstart(command, {
-        on_stdout = function(_, stdout)
-            if finished == false then
-                local string_value = table.concat(stdout)
-		local json_value = json.decode(string_value)
-
-		callback(json_value)
-            end
-
-	    finished = true
-        end,
-	on_stderr = function(_, stderr)
-            if stderr[0] ~= nil then
-                vim.api.nvim_echo({ { "Crates info retrieval failed.", "WarningMsg" } }, {}, {})
-            end
-        end,
-    })
+function Module.get_current_dependency_version(name)
+    local url = table.concat({"https://crates.io/api/v1/crates/", name})
+    local raw = curl.get(url, { accept = "application/json" })
+    return json.decode(raw).crate.max_stable_version
 end
 
 return Module
