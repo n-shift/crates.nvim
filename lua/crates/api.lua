41980ffb7e6430c7ec64790a43e97238448a22ec
shift-d <me@shift-d.tk>
2021-08-19 12:14:38 +0300

    chore: add callback

--- lua/crates/api.lua
+++ lua/crates/api.lua
@@ -4,7 +4,10 @@ local curl = require("plenary.curl")
 local Module = {}
 
 function Module.get_current_dependency_version(name)
-    local url = table.concat({"https://crates.io/api/v1/crates/", name})
+    local url = table.concat({"https://crates.io/api/v1/crates/", name, callback = function(res)
+        return res
+    end
+    })
     local raw = curl.get(url, { accept = "application/json", compressed = false, dump = {""} })
     return json.decode(raw).crate.max_stable_version
 end
